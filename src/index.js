dd2aa4a20264af7bebae1f3e52412c26d21bdd36
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-06 20:21:35 -0300

    feat: :sparkles: Atualizar Conta

--- src/index.js
+++ src/index.js
@@ -114,4 +114,19 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(200).json(statement);
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { name } = request.body;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
 app.listen(3333);
