8e6839b7b4a37d1a767247c924f6b8400636b450
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-06 17:47:19 -0300

    feat: :sparkles: Middlewares

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid (Universally Unique Identifier)
- * statement []
- */
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -37,15 +44,11 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+//app.use(verifyIfExistsAccountCPF()); // Se todas as rotas abaixo dele vão usar
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  
   return response.json(customer.statement);
 })
 
