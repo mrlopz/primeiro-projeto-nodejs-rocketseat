de7e0e2dc5f80cc43c5e79e2e2ff949ca87ef4fd
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-06 17:37:33 -0300

    feat: :sparkles: Validando CPF existente

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
+  }
+
   const id = uuidv4();
 
   customers.push({
